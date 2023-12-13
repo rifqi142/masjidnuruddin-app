1649c53c0af91d13ecb20cb45bf2acacc2207001
Muhammad Rifqi Setiawan <muhammadrifqi142@gmail.com>
2023-12-13 22:47:02 +0700

    add and design body detail al-quran pages

--- src/app/surat/[id]/page.jsx
+++ src/app/surat/[id]/page.jsx
@@ -5,11 +5,12 @@ import { ArrowLeft, ArrowRight } from "@phosphor-icons/react/dist/ssr";
 
 const DetailSurat = async ({ params: { id } }) => {
   const getDetailSurat = await getAlquranResponse(`surat/${id}`);
+  const indexSurat = getDetailSurat.data.nomor;
   return (
-    <section className="px-2 md:px-16 py-5">
+    <section className="px-2 md:px-16 py-5 bg-gray-10">
       <div className="w-full border-2 bg-green-30 rounded-2xl p-5 overflow-hidden">
         <div className="grid grid-cols-2 grid-rows-3">
-          <div className="gap-2 md:row-span-2 md:col-span-1 col-span-2">
+          <div className="gap-2 md:row-span-2 md:col-span-1 col-span-2  ">
             <h2 className="text-xl sm:text-2xl font-bold text-white">
               {getDetailSurat.data.namaLatin} - {getDetailSurat.data.nama}
             </h2>
@@ -19,7 +20,7 @@ const DetailSurat = async ({ params: { id } }) => {
             </p>
           </div>
 
-          <div className="col-span-2 md:row-span-2 md:col-span-1 flex items-end md:items-start justify-end">
+          <div className="col-span-2 md:row-span-2 md:col-span-1 flex items-end md:items-start justify-end  ">
             <Link href={`/tafsir/${id}`}>
               <div className="bg-yellow-60 rounded-md md:w-28 font-bold p-2 text-center text-sm hover:bg-yellow-30">
                 Lihat Tafsir
@@ -27,7 +28,7 @@ const DetailSurat = async ({ params: { id } }) => {
             </Link>
           </div>
 
-          <div className="col-span-2 mt-1">
+          <div className="col-span-2 mt-2  ">
             <audio
               controls
               className="audio-player bg-yellow-60 rounded-md w-full h-11 md:h-14 "
@@ -41,56 +42,67 @@ const DetailSurat = async ({ params: { id } }) => {
           </div>
         </div>
       </div>
-
-      {getDetailSurat.data.suratSelanjutnya === false ? null : (
-        <div>
-          <Link
-            href={`/surat/${getDetailSurat.data.suratSelanjutnya.nomor}`}
-            className="p-2"
-          >
-            <div className="flex flex-row gap-2 border border-red-500">
-              ({getDetailSurat.data.suratSelanjutnya.namaLatin}) Surah
-              Selanjutnya
-              <ArrowRight size={23} />
-            </div>
-          </Link>
-        </div>
-      )}
-
-      {getDetailSurat.data.suratSebelumnya === false ? null : (
-        <div>
+      {/* next, previous */}
+      <div className="mt-3 flex flex-col md:flex-row gap-2 text-white text-base justify-center ">
+        {getDetailSurat.data.suratSebelumnya === false ? null : (
           <Link
             href={`/surat/${getDetailSurat.data.suratSebelumnya.nomor}`}
-            className="p-2"
+            className="p-2 bg-green-30 rounded-lg h-10 flexCenter flex-1 hover:bg-green-50 "
           >
             <div className="flex flex-row gap-2">
               <ArrowLeft size={23} />(
               {getDetailSurat.data.suratSebelumnya.namaLatin}) Surah Sebelumnya
             </div>
           </Link>
-        </div>
-      )}
+        )}
 
-      <div>
-        <div>
-          {getDetailSurat.data.ayat.map((ayat, index) => (
-            <div key={index}>
-              <h5>{ayat.nomorAyat}</h5>
-              <div>
-                <h1>{ayat.teksArab}</h1>
-                <p>{ayat.teksLatin}</p>
-              </div>
-              <div>
-                <p>{ayat.teksIndonesia}</p>
-              </div>
-              <div>
-                <audio controls>
-                  <source src={ayat.audio["05"]} type="audio/mpeg" />
-                </audio>
-              </div>
+        {getDetailSurat.data.suratSelanjutnya === false ? null : (
+          <Link
+            href={`/surat/${getDetailSurat.data.suratSelanjutnya.nomor}`}
+            className="p-2 bg-green-30 rounded-lg h-10 flexCenter flex-1 hover:bg-green-50"
+          >
+            <div className="flex flex-row gap-2">
+              ({getDetailSurat.data.suratSelanjutnya.namaLatin}) Surah
+              Selanjutnya
+              <ArrowRight size={23} />
             </div>
-          ))}
-        </div>
+          </Link>
+        )}
+      </div>
+
+      <div className="mt-5">
+        {getDetailSurat.data.ayat.map((ayat, index) => (
+          <div
+            key={index}
+            className="w-full border-2 bg-white rounded-tr-lg rounded-bl-lg p-5 
+            overflow-hidden text-black mt-3 shadow-sm hover:shadow-lg"
+          >
+            <div className="flex flex-col">
+              <h4 className="text-xl">
+                {indexSurat} : {ayat.nomorAyat}
+              </h4>
+              <h1 className="text-3xl flexEnd mt-3 flex-wrap text-justify">
+                <span className="text-right text-[justify-last] leading-loose">
+                  {ayat.teksArab}
+                </span>
+              </h1>
+            </div>
+            <div className="mt-5 text-base md:text-xl text-justify text-yellow-80">
+              <p>{ayat.teksLatin}</p>
+            </div>
+            <div className="mt-3 text-sm md:text-lg text-justify">
+              <p>{ayat.teksIndonesia}</p>
+            </div>
+            <div className="mt-3">
+              <audio
+                controls
+                className="audio-player bg-yellow-60 rounded-md w-52"
+              >
+                <source src={ayat.audio["05"]} type="audio/mpeg" />
+              </audio>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="py-2 bg-dark-30 mt-10 p-2 md:p-2">
+    <footer className="py-2 bg-dark-30 p-2 md:p-2">
       <div className="flex justify-between mt-10 container mx-auto flex-col md:flex-row lg:px-20 xl:px-40">
         <div className="flex-1">
           <div className="flex flex-row">
